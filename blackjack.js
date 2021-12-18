7b69a57346361cefcd39f761d701ea2808371203
Joel Hernandez <joel62946@gmail.com>
2021-12-18 08:55:04 -0600

    trying to make a blackjack deck

--- blackjack.js
+++ blackjack.js
@@ -1,10 +1,17 @@
 import Deck from "./deck.js"
 
 const dealerCards = document.querySelector('.dealerCards');
+const myCards = document.querySelector('.myCards');
+const hitMe = document.querySelector('.hit');
 
+startGame()
+function startGame() {
+    for (let i = 0; i < 8; i++) {
+        new Deck();
+    }
+}
 
-const deck = new Deck()
-deck.shuffle()
-console.log(deck.cards);
-
+hitMe.addEventListener('click', () => {
+    myCards.appendChild(deck.cards.getHTML());
+})
 dealerCards.appendChild(deck.cards[0].getHTML());
--- deck.js
+++ deck.js
@@ -1,13 +1,18 @@
 const SUITS = ["♠", "♣", "♥", "♦"]
 const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
-
+function makeEight() {
+    let eightSuits = []
+    for (let i = 0; i < 8; i++) {
+        eightSuits += SUITS
+    }
+}
 export default class Deck {
     constructor(cards = freshDeck()) {
         this.cards = cards;
     }
 
     get numberOfCards() {
-        return this.cards.length
+        return this.cards.length;
     }
     pull() {
 
@@ -15,14 +20,13 @@ export default class Deck {
 
     shuffle() {
         for (let i = this.numberOfCards - 1; i > 0; i--) {
-            const newindex = Math.floor(Math.random() * (i + 1))
-            const oldValue = this.cards[newindex]
-            this.cards[newindex] = this.cards[i]
-            this.cards[i] = oldValue
+            const newindex = Math.floor(Math.random() * (i + 1));
+            const oldValue = this.cards[newindex];
+            this.cards[newindex] = this.cards[i];
+            this.cards[i] = oldValue;
         }
     }
 }
-
 class Card {
     constructor(suit, value) {
         this.suit = suit;
@@ -33,26 +37,26 @@ class Card {
         return this.suit === '♠' || this.suit === '♣' ? 'black' : 'red'
     }
     getHTML() {
-        const cardDiv = document.createElement('div')
-        const topDiv = document.createElement('div')
-        const centerDiv = document.createElement('div')
-        const bottomDiv = document.createElement('div')
-        cardDiv.classList.add("card", this.color)
-        topDiv.classList.add("top")
-        centerDiv.classList.add("center")
-        bottomDiv.classList.add("bottom")
-        cardDiv.append(topDiv, centerDiv, bottomDiv)
-        topDiv.innerText = `${this.value}${this.suit}`
-        centerDiv.innerText = `${this.suit}`
-        bottomDiv.innerText = `${this.suit}${this.value}`
-        return cardDiv
+        const cardDiv = document.createElement('div');
+        const topDiv = document.createElement('div');
+        const centerDiv = document.createElement('div');
+        const bottomDiv = document.createElement('div');
+        cardDiv.classList.add("card", this.color);
+        topDiv.classList.add("top");
+        centerDiv.classList.add("center");
+        bottomDiv.classList.add("bottom");
+        cardDiv.append(topDiv, centerDiv, bottomDiv);
+        topDiv.innerHTML = `<p>${this.value}</p><p>${this.suit}</p>`;
+        centerDiv.innerText = `${this.suit}`;
+        bottomDiv.innerHTML = `<p>${this.value}</p><p>${this.suit}</p>`;
+        return cardDiv;
     }
 }
 
 function freshDeck() {
     return SUITS.flatMap(suit => {
         return VALUES.map(value => {
-            return new Card(suit, value)
-        })
-    })
-} 
+            return new Card(suit, value);
+        });
+    });
+}
